9d72e3539c05ec0a9220080314dcd4bf51158352
Vakme <4bielak.aleksandra@gmail.com>
2022-11-18 22:17:58 +0100

    chore: setup path for gh actions

--- LandingPage/vite.config.ts
+++ LandingPage/vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CompassOfWomanhood/",
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin

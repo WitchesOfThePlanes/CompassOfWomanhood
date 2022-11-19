eda3f1e38eecebbc5df57c98439c64cf8b417797
Vakme <4bielak.aleksandra@gmail.com>
2022-11-19 23:00:26 +0100

    fix: colors

--- LandingPage/src/plugins/vuetify.ts
+++ LandingPage/src/plugins/vuetify.ts
@@ -11,16 +11,14 @@ import "vuetify/styles";
 // Composables
 import { createVuetify } from "vuetify";
 
-import colors from "vuetify/lib/util/colors";
-
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     themes: {
       light: {
         colors: {
-          primary: colors.deepPurple.base,
-          secondary: colors.grey.darken2,
+          primary: "#512da8",
+          secondary: "#616161",
         },
       },
     },

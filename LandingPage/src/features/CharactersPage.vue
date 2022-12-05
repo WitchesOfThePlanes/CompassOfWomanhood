e8fefa36f1a1b849a2e886fe4e44a6262becc65e
Vakme <4bielak.aleksandra@gmail.com>
2022-12-06 00:47:39 +0100

    feat: animations

--- LandingPage/src/features/CharactersPage.vue
+++ LandingPage/src/features/CharactersPage.vue
@@ -4,17 +4,29 @@
     class="d-flex flex-column align-center justify-center pa-10"
   >
     <div :class="smAndUp && 'page'">
-      <h2 class="ma-5">
-        <SectionTitle>
-          <img class="sword" :src="swordPng" />
-          {{ $t("CHARACTERS.TITLE") }}
-        </SectionTitle>
-      </h2>
-      <div class="mb-10 mx-5">
-        {{ $t("CHARACTERS.INTRO") }}
+      <div data-aos="fade-up">
+        <h2 class="ma-5">
+          <SectionTitle>
+            <img class="sword" :src="swordPng" />
+            {{ $t("CHARACTERS.TITLE") }}
+          </SectionTitle>
+        </h2>
+        <div class="mb-10 mx-5">
+          {{ $t("CHARACTERS.INTRO") }}
+        </div>
       </div>
-      <CharacterDetails character-name="SHITHRI" layout="left" />
-      <CharacterDetails character-name="CALY" layout="right" />
+      <CharacterDetails
+        data-aos="fade-up"
+        data-aos-delay="500"
+        character-name="SHITHRI"
+        layout="left"
+      />
+      <CharacterDetails
+        data-aos-delay="1000"
+        data-aos="fade-up"
+        character-name="CALY"
+        layout="right"
+      />
     </div>
   </div>
 </template>

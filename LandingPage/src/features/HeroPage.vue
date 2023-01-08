4189ec9bf3414a9eccda73ef982388103e94e82b
Vakme <4bielak.aleksandra@gmail.com>
2023-01-09 00:13:58 +0100

    fix: subtitle

--- LandingPage/src/features/HeroPage.vue
+++ LandingPage/src/features/HeroPage.vue
@@ -13,9 +13,6 @@
           <h1 class="align-self-center font-weight-medium text-center">
             <img class="logo" :src="logoImagePath" alt="logo" />
           </h1>
-          <h4 class="align-self-end text-right subtitle">
-            {{ $t("HERO.SUBTITLE") }}
-          </h4>
         </header>
       </div>
       <div

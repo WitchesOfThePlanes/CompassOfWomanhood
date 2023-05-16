2fb29444997877e5a3ecae98060948e8917eabaa
Misha Kotwica <udiknedormin@gmail.com>
2023-05-16 21:53:21 +0200

    Caly B with Jaheira & Korgan

--- CompassOfWomanhood/dialogues/caly/B6WCALY.d
+++ CompassOfWomanhood/dialogues/caly/B6WCALY.d
@@ -18,6 +18,44 @@ BEGIN B6WCALY
 // Jaheira
 //
 
+CHAIN IF ~
+  IfValidForPartyDialogue("6WCALY")
+  IfValidForPartyDialogue("Jaheira")
+  CombatCounter(0)
+  See("Jaheira")
+  // their banters don't have any particular order
+  Global("6W#banter_Caly_Jaheira_0","GLOBAL",0)
+~ THEN B6WCALY 6W#banter_caly_jaheira_0
+  @1020000 /* I consider every religion and every mindset important and worth listening to. */
+  DO ~SetGlobal("6W#banter_Caly_Jaheira_0","GLOBAL",1)~
+  == BJAHEIR
+  @1020001 /* I consider you to have too much time on your hands, child. */
+  == B6WCALY
+  @1020002 /* But haven't you ever pondered the nature of things and the seed of truth in every faith and culture? */
+  == BJAHEIR
+  @1020003 /* You have much to learn about the harsh reality. */
+  == B6WCALY
+  @1020004 /* Truly, the world is harsh, but isn't it wonderful too? */
+EXIT
+
+CHAIN IF ~
+  IfValidForPartyDialogue("6WCALY")
+  IfValidForPartyDialogue("Jaheira")
+  CombatCounter(0)
+  See("Jaheira")
+  // their banters don't have any particular order
+  Global("6W#banter_Caly_Jaheira_1","GLOBAL",0)
+~ THEN B6WCALY 6W#banter_caly_jaheira_1
+  @1020100 /* My lady, I wanted to say that I really admire that you took our party under your wings. */
+  DO ~SetGlobal("6W#banter_Caly_Jaheira_1","GLOBAL",1)~
+  == BJAHEIR
+  @1020101 /* I prefer a bear's paws rather than bird's wings, my child. And an embrace rather than flight. Like the Eartmother herself. */
+  == B6WCALY
+  @1020102 /* Oh. I never... that thought is so foreign to me. Like a different Plane, almost. */
+  =
+  @1020103 /* I will think about it. */
+EXIT
+
 
 
 //
@@ -142,6 +180,51 @@ EXIT
 // Korgan
 //
 
+CHAIN
+  IfValidForPartyDialogue("6WCALY")
+  IfValidForPartyDialogue("Korgan")
+  See("6WCALY")
+  Global("6W#banter_Caly_Korgan","GLOBAL",0)
+~ THEN B6WCALY 6W#banter_caly_korgan_0
+  @1110000 /* Oh Korgan, that enormous axe of yours is indeed very impressive. Is it some form of compensation? */
+  DO ~SetGlobal("6W#banter_Caly_Korgan","GLOBAL",1)~
+  == BKORGAN
+  @1110001 /* Har har, it's a promise! */
+  == B6WCALY
+  @1110002 /* Let me make a guess. Does it symbolize how you like to finish your undertakings in one stroke? */
+  == BKORGAN
+  @1110003 /* It's nae about how many there are, but what a stroke that is makes all the difference. If it makes blood run hot, it's good. */
+  == B6WCALY
+  @1110004 /* “Fleeting” might be the word you are looking for. */
+  == BKORGAN
+  @1110005 /* Oh, ye're sayin' it as if wenches nae like that! */
+  == B6WCALY
+  @1110006 /* They surely do. As do many women who enjoy the gloss of coins. */
+  == BKORGAN
+  @1110007 /* As do I! Fast strokes, shining gold. And nae wasting me time. */
+  == B6WCALY
+  @1110008 /* Wasting time... I can see it could be called that in your case, indeed. Thank you for an exhaustive lecture. */
+  == BKORGAN
+  @1110009 /* Any time, lass! */
+EXIT
+
+CHAIN
+  IfValidForPartyDialogue("6WCALY")
+  IfValidForPartyDialogue("Korgan")
+  See("6WCALY")
+  Global("6W#banter_Caly_Korgan_light","GLOBAL",0)
+~ THEN B6WCALY 6W#banter_caly_korgan_light_0
+  @1111000 /* If I speak with devas and unicorns kneel before me, but do not have love, I am only a clanging copper. */
+  DO ~SetGlobal("6W#banter_Caly_Korgan","GLOBAL",1)~
+  == BKORGAN
+  @1111001 /* Yer face ain't yellow enough fer ye t' speak like that. */
+  == B6WCALY
+  @1111002 /* I was but quoting psalm 21:37, for the glory of our Lady, Selûne. */
+  == BKORGAN
+  @1111003 /* That goddess o' yers knows nah business. */
+  == B6WCALY
+  @1111004 /* She has no need to, you simpleton. */
+EXIT
 
 
 //
